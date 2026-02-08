834b6ba95452f5f31855c1aac8c0ec31c6866cf2
MuTEn-R0sHi <MuTEn-R0sHi@users.noreply.github.com>
2026-02-08 01:45:50 +0100

    fix: add mockrobloxglobals to net test-setup for typeof polyfill

--- packages/net/test-setup.ts
+++ packages/net/test-setup.ts
@@ -1,5 +1,14 @@
 // Polyfill for Roblox types in Node environment
+import { mockRobloxGlobals } from "@rbx/testing";
+
+mockRobloxGlobals();
+
 // @ts-expect-error - Polyfilling native prototype for tests
 String.prototype.size = function () {
+  return this.length;
+};
+
+// @ts-expect-error - Polyfilling native prototype for tests
+Array.prototype.size = function () {
   return this.length;
 };
